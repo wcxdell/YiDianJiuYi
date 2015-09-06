3aa96cbe7bd1599aa1c1366f0ee1a01be939e6ba
appel <appel@appeldeMacBook-Air.local>
2015-09-06 10:44:05 +0800

    Initial Commit

---
+++ Mobile Medical Treatment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mobile Medical Treatment
+//
+//  Created by appel on 15/9/6.
+//  Copyright (c) 2015年 appel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
